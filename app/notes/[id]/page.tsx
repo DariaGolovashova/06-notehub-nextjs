f868a29bf64f647680a0eec5ec83c45c8de7bd91
Daria Golovashova <darja.golovashova@gmail.com>
2026-04-10 20:54:30 +0200

    page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,4 +1,4 @@
-// import { fetchNoteById } from "@/lib/api";
+import { fetchNoteById } from "@/lib/api";
 import { fetchNotes } from "@/lib/api";
 import NotesClient from "../Notes.client";
 import {
@@ -8,29 +8,17 @@ import {
 } from "@tanstack/react-query";
 import NoteDetailsClient from "./NoteDetails.client";
 
-// interface PageProps {
-//   params: Promise<{ id: string }>;
-// }
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
 
-// async function NoteDetailsPage({ params }: PageProps) {
-//   const { id } = await params;
-//   const queryClient = new QueryClient();
-
-//   await queryClient.prefetchQuery({
-//     queryKey: ["note", id],
-//     queryFn: () => fetchNoteById(id),
-//   });
-async function NoteDetailsPage() {
+async function NoteDetailsPage({ params }: PageProps) {
+  const { id } = await params;
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ["notes", "", 1],
-    queryFn: () =>
-      fetchNotes({
-        search: "",
-        page: 1,
-        perPage: 10,
-      }),
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
   });
 
   return (
